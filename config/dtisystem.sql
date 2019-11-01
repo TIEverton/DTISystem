f04902b1b73dd44af9e540aa7355fe1d509f1f22
Alisson <as119459@gmail.com>
2019-11-01 08:09:47 -0300

    Upadate Banco de Dados

--- config/dtisystem.sql
+++ config/dtisystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 28-Out-2019 às 19:26
+-- Generation Time: 01-Nov-2019 às 12:09
 -- Versão do servidor: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -25,6 +25,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `agrupamento`
+--
+
+CREATE TABLE `agrupamento` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `campus`
 --
 
@@ -33,16 +44,8 @@ CREATE TABLE `campus` (
   `nome` varchar(200) NOT NULL,
   `cnpj` varchar(200) NOT NULL,
   `endereco` varchar(200) NOT NULL,
-  `rua` varchar(200) NOT NULL,
-  `numero` varchar(200) NOT NULL
+  `bairro` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `campus`
---
-
-INSERT INTO `campus` (`id`, `nome`, `cnpj`, `endereco`, `rua`, `numero`) VALUES
-(2, 'teste teste', 'teste', 'teste', 'teste', 'teste');
 
 -- --------------------------------------------------------
 
@@ -52,20 +55,41 @@ INSERT INTO `campus` (`id`, `nome`, `cnpj`, `endereco`, `rua`, `numero`) VALUES
 
 CREATE TABLE `equipamento` (
   `id` int(11) NOT NULL,
-  `identificador` varchar(300) NOT NULL,
-  `nome` varchar(200) NOT NULL,
-  `quantidade` int(250) NOT NULL,
-  `descricao` varchar(300) NOT NULL,
-  `campus` int(11) NOT NULL
+  `numeracao` varchar(100) NOT NULL,
+  `agrupamento` int(11) NOT NULL,
+  `campus` int(11) NOT NULL,
+  `descricao` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Extraindo dados da tabela `equipamento`
+-- Estrutura da tabela `reserva`
 --
 
-INSERT INTO `equipamento` (`id`, `identificador`, `nome`, `quantidade`, `descricao`, `campus`) VALUES
-(1, 'tombo', 'nome', 6, 'desc', 2),
-(2, 'tombo', 'nome22222', 6, 'desc', 2);
+CREATE TABLE `reserva` (
+  `id` int(11) NOT NULL,
+  `campus` int(11) NOT NULL,
+  `sala` int(11) NOT NULL,
+  `equipamento` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `turno` varchar(30) NOT NULL,
+  `horario` varchar(10) NOT NULL,
+  `observacoes` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sala`
+--
+
+CREATE TABLE `sala` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  `campus` int(11) NOT NULL,
+  `situacao` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -95,10 +119,42 @@ INSERT INTO `usuarios` (`id`, `nome`, `login`, `senha`, `email`, `cpf`, `nivel`)
 --
 
 --
+-- Indexes for table `agrupamento`
+--
+ALTER TABLE `agrupamento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `campus`
+--
+ALTER TABLE `campus`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `id_2` (`id`);
+
+--
 -- Indexes for table `equipamento`
 --
 ALTER TABLE `equipamento`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `agrupamento` (`agrupamento`),
+  ADD KEY `campus` (`campus`);
+
+--
+-- Indexes for table `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `campus` (`campus`),
+  ADD KEY `equipamento` (`equipamento`),
+  ADD KEY `sala` (`sala`);
+
+--
+-- Indexes for table `sala`
+--
+ALTER TABLE `sala`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `campus` (`campus`);
 
 --
 -- Indexes for table `usuarios`
@@ -111,16 +167,65 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT for table `agrupamento`
+--
+ALTER TABLE `agrupamento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `campus`
+--
+ALTER TABLE `campus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `equipamento`
 --
 ALTER TABLE `equipamento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sala`
+--
+ALTER TABLE `sala`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `equipamento`
+--
+ALTER TABLE `equipamento`
+  ADD CONSTRAINT `equipamento_ibfk_1` FOREIGN KEY (`agrupamento`) REFERENCES `agrupamento` (`id`),
+  ADD CONSTRAINT `equipamento_ibfk_2` FOREIGN KEY (`campus`) REFERENCES `campus` (`id`);
+
+--
+-- Limitadores para a tabela `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`campus`) REFERENCES `campus` (`id`),
+  ADD CONSTRAINT `reserva_ibfk_2` FOREIGN KEY (`equipamento`) REFERENCES `equipamento` (`id`),
+  ADD CONSTRAINT `reserva_ibfk_3` FOREIGN KEY (`sala`) REFERENCES `sala` (`id`);
+
+--
+-- Limitadores para a tabela `sala`
+--
+ALTER TABLE `sala`
+  ADD CONSTRAINT `sala_ibfk_1` FOREIGN KEY (`campus`) REFERENCES `campus` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
