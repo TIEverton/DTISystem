fc0cc05c63d01cd58bad4056239a2d6e2b534aac
Everton Pinheiro <44790240+TIEverton@users.noreply.github.com>
2019-11-26 19:50:16 -0300

    Atualização no Front-End da tela de reserva

--- view/views/cadastroReserva.php
+++ view/views/cadastroReserva.php
@@ -18,7 +18,7 @@ require_once '../../config/DB.php';
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="../../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <title>DTI - Cadastro Equipamento &copy;</title>
+    <title>DTI - Reservar Equipamento &copy;</title>
 </head>
 <body>
 <!-- INCLUDE MENU -->
@@ -31,23 +31,21 @@ require_once '../../config/DB.php';
 <div class="row justify-content-center">
   <div class="col-8">
     <div class="form-group" style="margin-top: 3%;">
-
+        <center><h4 id="tituloCards">RESERVA DE EQUIPAMENTO</h4>
+          <a href="" id="duvidasLink">Dúvidas sobre a reserva, clique aqui!</a>                      
+        </center>
     <div class="accordion" id="accordionExample">
       <!-- CARD 1 -->
       <div class="card">
         <!-- Titulo do Card -->
-        <div class="card-header" id="headingOne">
-            <h2 class="mb-0">
-                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                  <i class='material-icons left'>location_city</i>Localização
-                </button>
-            </h2>
+        <div class="card-header" id="headingOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+            <div id="tituloCard1"><i class='material-icons'>location_city</i><p id="nameCard">Campus:</p></div>
         </div>
 
         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
         <div class="card-body">
                 <div class="form-group" >
-                    Campus:
+                    <b>Campus:</b>
                     <div class="input-group mb-3">
                     <span class="input-group-text" ><i class='material-icons left'>location_city</i></span>
                     <select class="form-control" id="select_campus" name="select_campus">
@@ -66,7 +64,7 @@ require_once '../../config/DB.php';
                     </select>
                     </div>
               
-                    Sala:
+                    <b>Sala:</b>
                     <div class="input-group mb-3">
                       <span class="input-group-text" ><i class='material-icons left'>people</i></span>
                     <select class="form-control" name="select_salas" id="select_salas">
@@ -93,24 +91,23 @@ require_once '../../config/DB.php';
 
           <!-- CARD 2 -->
           <div class="card">
-            <div class="card-header" id="headingTwo">
-              <h2 class="mb-0">
-                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                <i class='material-icons left'>schedule</i> Diaaas
-                </button>
-              </h2>
+            <div class="card-header" id="headingTwo" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              <div id="tituloCard1">
+                  <i class='material-icons left'>date_range</i><p id="nameCard2">Data e Horário:</p>
+              </div>
+        
             </div>
 
           <div id="collapseTwo" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
             <div class="card-body">
                 <div class="form-group" >
-                Data:
+                <b>Data:</b>
               <div class="input-group mb-3">
                     <span class="input-group-text" ><i class='material-icons left'>date_range</i></span>
                     <input type="date" name="data" class="form-control" placeholder="Escolha uma data." id="data">
               </div>
         
-              Turno:
+              <b>Turno:</b>
               <div class="input-group mb-3">
                     <span class="input-group-text" ><i class='material-icons left'>schedule</i></span>
               <select class="form-control" name="select_turno" id="select_turno">
@@ -121,18 +118,18 @@ require_once '../../config/DB.php';
               </select>
               </div>
         
-              Horário:
+              <b>Horário:</b>
               <div class="input-group mb-3">
               <span class="input-group-text" ><i class='material-icons left'>schedule</i></span>
               <select class="form-control" name="select_horario" id="select_horario">
-                  <option>Selecione uma Horário:</option>
+                  <option>Selecione horário inicial:</option>
                   <option>AB</option>
                   <option>CD</option>
               </select>
               &nbsp;&nbsp;&nbsp;
               <span class="input-group-text" ><i class='material-icons left'>schedule</i></span>
               <select class="form-control" name="select_horario" id="select_horario">
-                  <option>Selecione uma Horário:</option>
+                  <option>Selecione horário final:</option>
                   <option>AB</option>
                   <option>CD</option>
               </select>
@@ -158,18 +155,17 @@ require_once '../../config/DB.php';
             <!-- CARD 3 -->
             <div class="card">
         <!-- Titulo do Card -->
-          <div class="card-header" id="headingThree">
-            <h2 class="mb-0">
-              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-              <i class='material-icons left'>location_city</i>Equipamento
-              </button>
-            </h2>
+          <div class="card-header" id="headingThree" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            <div id="tituloCard3">
+                <i class='material-icons left'>keyboard</i><p id="nameCard3">Equipamento</p>
+            </div>
           </div>
 
         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
         <div class="card-body">
-                <div class="form-group" >
-                Agrupamento:
+              <div class="form-group" >
+              
+              <b>Agrupamento:</b>
               <div class="input-group mb-3">
                       <span class="input-group-text" ><i class='material-icons left'>keyboard</i></span>
               <select class="form-control" name="select_equipamentos" id="select_equipamentos">
@@ -177,7 +173,7 @@ require_once '../../config/DB.php';
               </select>
               </div>
               
-              Equipamento:
+              <b>Equipamento:</b>
               <div class="input-group mb-3">
                     <span class="input-group-text" ><i class='material-icons left'>keyboard</i></span>
               <select class="form-control" name="select_equipamentos" id="select_equipamentos">
@@ -186,18 +182,18 @@ require_once '../../config/DB.php';
               </div>
               
                             
-                    Observação:
-                    <div class="input-group mb-3">
-                          <div class="input-group-prepend">
-                          <span class="input-group-text" ><i class='material-icons left'>insert_comment</i></span>
-                          </div>
-                          <textarea id="observacao" class="form-control" id="exampleFormControlTextarea1" rows="1" name="observacao"></textarea>
+              <b>Observação:</b>
+              <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                    <span class="input-group-text" ><i class='material-icons left'>insert_comment</i></span>
                     </div>
-                    
-                    <input type="hidden" name="acao" class="form-control" value="inserir"/>
-                    <input type="hidden" name="responsavel" value="<?php echo $_SESSION['user_id'];?>"/>
-                  </div>
-                </div>
+                    <textarea id="observacao" class="form-control" id="exampleFormControlTextarea1" rows="1" name="observacao"></textarea>
+              </div>
+              
+              <input type="hidden" name="acao" class="form-control" value="inserir"/>
+              <input type="hidden" name="responsavel" value="<?php echo $_SESSION['user_id'];?>"/>
+              </div>
+              </div>
         </div>
 
       </div>
@@ -211,7 +207,7 @@ require_once '../../config/DB.php';
           <button type="submit" class="btn btn-success"><i class="fa fa-floppy-o" aria-hidden="true"></i> Salvar</button></a>
           <button type="reset" class="btn btn-warning"><i class="fa fa-eraser" aria-hidden="true"></i> Limpar</button></a>
           <a href="listarReserva.php"><button type="button" class="btn btn-primary"><i class="fa fa-search" aria-hidden="true"></i> Pesquisar</button></a>
-      </div>
+        </div>
     </div>
   </div>
 </div>
@@ -319,6 +315,7 @@ require_once '../../config/DB.php';
   })
   
 </script>
+
 
 </body>
 </html>
