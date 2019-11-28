d21d4fe2f100846b91089eeb2a77e6f0f9db0f5c
Everton Pinheiro <44790240+TIEverton@users.noreply.github.com>
2019-11-28 13:51:28 -0300

    v1 Tela Recuperar Senha

--- view/views/recuperar.php
+++ view/views/recuperar.php
@@ -17,7 +17,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <title>DTI - Login &copy;</title>
+    <title>DTI - Recuperar &copy;</title>
 </head>
 <body>
 <form method="POST" action="../../controller/usuario/Usuario.controller.php">
@@ -30,27 +30,27 @@
 
             <div class="p-3 card">
                 <div class="card-body">
-                        <h2 class="text-login">ENTRAR</h2>
+                        <h2 class="text-login">RECUPERAR ACESSO</h2>
                     <div class="form-group">
                         <div class="input-group mb-3">
                                 <div class="input-group-prepend">
                                 <span class="input-group-text" id="basic-addon1"><i class='material-icons left'>email</i></span>
                                 </div>
-                                <input type="email" name="email" class="form-control" placeholder="Seu email"  aria-label="Username" aria-describedby="basic-addon1" required>
+                                <input type="email" name="email" class="form-control" placeholder="Digite seu email"  aria-label="Username" aria-describedby="basic-addon1" required>
                         </div>
 
                         <div class="input-group mb-3">
                                 <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1"><i class='material-icons left'>lock</i></span>
+                                <span class="input-group-text" id="basic-addon1"><i class='material-icons left'>call_to_action</i></span>
                                 </div>
-                                <input type="password" name="senha" class="form-control"  placeholder="Sua senha" aria-label="Password" aria-describedby="basic-addon1" required>
+                                <input type="password" name="senha" class="form-control"  placeholder="Digite seu CPF" aria-label="Password" aria-describedby="basic-addon1" required>
                         </div>
                         
                         <input type="hidden" id="acao" name="acao" value="autenticar">
-                        <a href="recuperar.php" id="recuperar-senha">Solicitar recuperação de login</a>
+                        <br>
                         <div class="botaoentrar">
 
-                            <button type="submit" class="btn btn-primary">Entrar</button>
+                            <button type="submit" class="btn btn-primary">Solicitar</button>
                         </div>
                     </div>
             </div>
